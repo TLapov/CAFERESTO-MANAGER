4b9bd3c00defd459ceafe5904dcb5bdebd303eca
Tlapov <toni.lapov95@gmail.com>
2024-09-29 12:49:08 +0200

    Initialize project structure for backend code

---
+++ server/src/config/environment.config.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const BASE_PATH: string = '/';
+const API: string = `/api/${process.env.VERSION}`;
+
+const PORT: number = Number(process.env.PORT);
+const HOST: string = process.env.HOST || 'localhost';
+
+const DB_PORT = Number(process.env.DB_PORT);
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_DATABASE = process.env.DB_DATABASE;
+
+export {
+    BASE_PATH,
+    API,
+    PORT, 
+    HOST,
+    DB_PORT,
+    DB_HOST,
+    DB_PASSWORD,
+    DB_USER,
+    DB_DATABASE
+}
